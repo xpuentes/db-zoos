a571f8719eec7cbd549af58810ce88895ffe7d0f
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-14 22:16:16 -0500

    Added knex config

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3',
+  },
+  useNullAsDefault: true,
+});
 
 const server = express();
 
@@ -7,7 +16,6 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-//
 
 const port = 3300;
 server.listen(port, function() {
