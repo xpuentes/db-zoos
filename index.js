ac00d871c4f62e305fa7e37b6d663a4bc819d2a9
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-14 22:28:14 -0500

    Added post endpoint

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+  const name = req.body;
+
+  db.insert(name)
+    .into('zoos')
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Unable to add new zoo.'});
+    })
+});
 
 const port = 3300;
 server.listen(port, function() {
